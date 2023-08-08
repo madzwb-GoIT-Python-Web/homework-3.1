cb9b933f898f4650c7ec3e94d515757b77b8789c
Volodymyr Zavatsky <volodymyr.zawatsky@gmail.com>
2023-08-08 20:55:32 +0300

    Added to argparser, use_names parameters. Renamed destiantion to name.

--- src/filesorter/_argparser.py
+++ src/filesorter/_argparser.py
@@ -1,12 +1,13 @@
 import argparse
 from pathlib import Path
 
-args = None
+_parser = None
+_args    = None
 
-def parse_args():
-    global args
-    if args is not None:
-        return args
+def get_parser():
+    # global _args
+    # if _args is not None:
+    #     return _args
     """Parse comand-line options"""
 
     parser = argparse.ArgumentParser(
@@ -18,12 +19,12 @@ def parse_args():
     )  # ,exit_on_error=False
     parser.add_argument(
         "directories",
-        help    = "Directories list to process, if not specified used current directory.",
-        type    = Path,
-        default = Path().cwd(),
-        action  = "store",
-        required= False,
-        nargs   = '*'
+        help        = "Directories list to process, if not specified used current directory.",
+        type        = Path,
+        action      = "store",
+        default     = Path().cwd(),
+        # required    = False,
+        nargs       = '*'
     )
     parser.add_argument(
         "-k", "--keep-empty-dir",
@@ -33,7 +34,7 @@ def parse_args():
         required    = False
     )
     parser.add_argument(
-        "-n", "--normalize",
+        "-norm", "--normalize",
         help        = "Normalize file and directory(for unpacking archives) names.",
         action      = "store_true",
         default     = False,
@@ -65,48 +66,47 @@ def parse_args():
         required    = False,
         nargs='?'
     )
+    # parser.add_argument(
+    #     "-v", "--verbose",
+    #     help        = ("increase output verbosity.\n\
+    # 0 - Only errors printout\n\
+    # 1 - Add warnings\n\
+    # 2 - Add destination directory creation\n\
+    # 3 - Add all filesystem modification\n\
+    # 4 - Add internal script structures"),
+    #     choices     =   [
+    #                         "critical",
+    #                         "error",
+    #                         "warning",
+    #                         "info",
+    #                         "debug",
+    #                         "notset"
+    #                     ],
+    #     # action="count",
+    #     action      = "store",
+    #     required    = False,
+    #     default     = "notset",
+    #     nargs       = '*'
+    # )
     parser.add_argument(
-        "-u", "--use",
-        type        = str.lower,
-        help        = "Individual executor.",
-        choices     =   [
-                            "mainthread",
-                            "thread",
-                            "process",
-                            "threads",
-                            "processes",
-                            "threadpool",
-                            "processpool"
-                        ],
-        metavar     = "ext_executor",
-        dest        = "ext_executor",
+        "-d", "--destination",
+        help        = "Destination directory.",
+        type        = Path,
+        metavar     = "destination",
         action      = "store",
-        default     = "mainthread",
-        require     = False,
-        nargs='?'
+        default     = Path().cwd(),
+        # default     = None,
+        required    = False
     )
-
     parser.add_argument(
-        "-v", "--verbose",
-        help        = ("increase output verbosity.\n\
-    0 - Only errors printout\n\
-    1 - Add warnings\n\
-    2 - Add destination directory creation\n\
-    3 - Add all filesystem modification\n\
-    4 - Add internal script structures"),
-        choices     =   [
-                            "critical",
-                            "error",
-                            "warning",
-                            "info",
-                            "debug",
-                            "notset"
-                        ],
-        # action="count",
-        action      = "store",
-        required    = False,
-        default     = "notset",
-        nargs       = '*'
+        "-un", "--use-names",
+        help        = "Using names as subdirectories.",
+        # type        = bool,
+        # metavar     = "use_names",
+        action      = "store_true",
+        default     = True,
+        # default     = None,
+        required    = False
     )
 
     group = parser.add_mutually_exclusive_group()
@@ -119,16 +119,17 @@ def parse_args():
         default     = "settings.json",
         required    = False
     )
-    group.add_argument(
-        "-d", "--destination",
-        help        = "Destination directory.",
-        type        = Path,
-        metavar     = "destination",
+    filter_group = parser.add_argument_group("Filter")
+    filter_group.add_argument(
+        "-n", "--name",
+        help        = "Filter name.",
+        type        = str,
+        metavar     = "name",
         action      = "store",
-        default     = None,
+        default     = "",#Path().cwd(),
         required    = False
     )
-    parser.add_argument(
+    filter_group.add_argument(
         "-e", "--extensions",
         help        = "File's extensions. Ex: 'jpg jpeg png bmp'",
         metavar     = "extensions",
@@ -137,7 +138,7 @@ def parse_args():
         required    = False,
         nargs       = '*'
     )
-    parser.add_argument(
+    filter_group.add_argument(
         "-f", "--functions",
         help        = "Functions' list(order sensitive).\n\
     A list of next functions:\n\
@@ -157,5 +158,45 @@ def parse_args():
                         ],
         nargs       = '*'
     )
-    args = parser.parse_args()
-    return args
+    filter_group.add_argument(
+        "-u", "--use",
+        type        = str.lower,
+        help        = "Individual executor.",
+        choices     =   [
+                            "mainthread",
+                            "thread",
+                            "process",
+                            "threads",
+                            "processes",
+                            "threadpool",
+                            "processpool"
+                        ],
+        metavar     = "ext_executor",
+        dest        = "ext_executor",
+        action      = "store",
+        default     = "mainthread",
+        required    = False,
+        nargs='?'
+    )
+
+    return parser
+
+def get_args(*args):
+    global _args, _parser
+    if _parser is None:
+        _parser = get_parser()
+    __args = _parser.parse_args(args)
+    # [source for source in args.directories if source == args.destination]
+    if not __args.use_names:
+        __args.use_names =  __args.destination in __args.directories        \
+                                if  isinstance(__args.directories, list)    \
+                                else                                        \
+                            __args.destination == __args.directories        \
+                                if  isinstance(__args.directories, Path)    \
+                                else                                        \
+                            __args.use_names
+    return __args
+
+def parse(*args):
+    _args = get_args(args)
+    return _args
--- src/filesorter/filters.py
+++ src/filesorter/filters.py
@@ -12,14 +12,14 @@ class Filter:
     def __init__(
             self,
             parent      : Filters|None,
-            destination : Path,
-            extensions  : str|list,
-            functions   : str|list,
+            name        : str,
+            extensions  : str|list[str],
+            functions   : str|list[str],
             use         : str = ""
         ):
         self.parent :Filters|None   = parent
-        self.root   :Path|None      = None      #   Root directory. Sets with adding Filter to Task.
-        self.destination    = destination   #   Destination directory.
+        self.root   :Path|None      = None  #   Root directory. Sets with adding Filter to Task.
+        self.name           = name          #   Destination directory.
         self.use            = use           #   Executor
         self._actions       = []            #   List of functions' objects.
 
@@ -47,9 +47,10 @@ class Filter:
             _action =   action(
                             path,
                             self.root,
-                            self.destination,
-                            self.parent.normalize if self.parent is not None else True,
-                            self.parent.overwrite if self.parent is not None else True
+                            self.name,
+                            self.parent.normalize if self.parent is not None else False,
+                            self.parent.overwrite if self.parent is not None else False,
+                            self.parent.use_names if self.parent is not None else True
                         )
             result.append(_action)
         if len(result) > 1:
@@ -66,29 +67,32 @@ class Filters(UserDict):
             path            : Path|str|None = None,
             filter          : Filter|None = None,
             keep_empty_dir  : bool = False,
-            normalize       : bool = True,
-            overwrite       : bool = True
+            normalize       : bool = False,
+            overwrite       : bool = False,
+            use_names       : bool = True
         ):
         super().__init__()
         self._root = None
         self.keep_empty_dir = keep_empty_dir
         self.normalize      = normalize     #   Normalize files' names.
         self.overwrite      = overwrite     #   Overwrite files in destination directory.
+        self.use_names      = use_names
 
         if isinstance(path, str):
-            if not len(path):
-                path = Path().cwd()
-            else:
+            # if not len(path):
+            #     path = Path().cwd()
+            # else:
                 path = Path(path)
-        if path and path.exists():
+        # if path and not path.exists():
+        #     path.mkdir()
             self._root    = path
-        else:   #   Before running protection
-            raise FileExistsError(f"Path: '{path}' doesn't exists.")
+        # else:   #   Before running protection
+        #     raise FileExistsError(f"Path: '{path}' doesn't exists.")
         
         # self._filters       = {}  #   Destination path to Filter mapping ex. {"archives": Filter()}.
         self._ext2filter    = {}    #   File extension to Filter mapping ex. {"zip": Filter()}.
         if filter and self._root:
-            self.data[filter.destination] = filter
+            self.data[filter.name] = filter
             filter.root = self._root
             for ext in filter.extensions:
                 self._ext2filter[ext] = filter
@@ -124,7 +128,17 @@ class Filters(UserDict):
 
     def __iadd__(self, _filter: Filter):
         """Add filter."""
-        self.data[_filter.destination] = _filter
+        # name = str(_filter.name)
+        if _filter.name in self.data:
+            extensions = self.data[str(_filter.name)].extensions
+            _filter.extensions.extend(extensions)
+            _filter.extensions = [*set(extensions)]
+
+            functions = self.data[str(_filter.name)].functions
+            _filter.functions.extend(functions)
+            _filter.functions = [*set(functions)]
+
+        self.data[_filter.name] = _filter
         _filter.root    = self._root
         _filter.parent  = self
         for ext in _filter.extensions:
@@ -136,7 +150,7 @@ class Filters(UserDict):
         """Remove filter."""
         _filter.root    = None
         _filter.parent  = None
-        self.data.pop(_filter.destination)
+        self.data.pop(_filter.name)
         for ext in _filter.extensions:
             self._ext2filter.pop(ext)
         return self
